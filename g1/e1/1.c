90846e3a7fb3ca5f3f879445bbe6ee7fafa7cad3
juancx25 <juancouselo25@gmail.com>
2023-01-18 22:40:45 -0300

    First commit of the AYED 2023 repository - First steps in Guide 1. - Charset ADT created and partially implemented.

---
+++ g1/e1/1.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    const char* symbols = ".,;?!";
+    char* phrase = "This, is a phrase; whose main purpose is to count punctuation signs. Is it well done? We will see!";
+
+    int r = count_appearances(phrase,symbols);
+
+    printf("El resultado es: %d\n",r);
+    return 0;
+}
